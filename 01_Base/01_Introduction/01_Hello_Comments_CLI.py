60c70421141eed07d0eaac8317a6ff339dbb136c
Rainer <github@rainerschroeder.com>
2023-07-06 16:06:10 +0200

    added further chapters

--- 01_Base/01_Introduction/01_Hello_Comments_CLI.py
+++ 01_Base/01_Introduction/01_Hello_Comments_CLI.py
@@ -2,8 +2,8 @@
 
 """
 This is a Multiline-Comment.
-Run this file in the terminal, just type:
-python3 "01_Hello_Comments_CLI.py"
+To run this file in the terminal, just go to the corresponding folder and type:
+python3 01_Hello_Comments_CLI.py
 """
 
 print("___Ʌ__________Ʌ__________Ʌ__________Ʌ__________Ʌ__________Ʌ___\n")
