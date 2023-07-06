04eeabf05e5e6d088e2523df24958ee524c20422
Rainer <github@rainerschroeder.com>
2023-07-06 15:26:30 +0200

    init

---
+++ 01_Base/01_Introduction/01_Hello_Comments_CLI.py
@@ -0,0 +1,15 @@
+# Welcome to the Python-World. This is a Single-Line-Comment in Python
+
+"""
+This is a Multiline-Comment.
+Run this file in the terminal, just type:
+python3 "01_Hello_Comments_CLI.py"
+"""
+
+print("___Ʌ__________Ʌ__________Ʌ__________Ʌ__________Ʌ__________Ʌ___\n")
+
+
+print("Hello Python-World!")
+
+
+print("\n_______________________________________________________________")
